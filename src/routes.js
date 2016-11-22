15551fc6f48f66855c7bc3f2812e328b1c901a38
keyeh <keyeh@users.noreply.github.com>
2016-11-22 09:26:51 +0000

    turn node_show into container

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import NodesIndex from './components/nodes_index';
 import NodesNew from './components/nodes_new';
-import NodesShow from './components/nodes_show';
+import NodeShow from './containers/node_show';
 
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={NodesIndex} />
         <Route path="nodes/new" component={NodesNew} />
-        <Route path="nodes/:node_id" component={NodesShow} />
+        <Route path="nodes/:node_id" component={NodeShow} />
     </Route>
 );
