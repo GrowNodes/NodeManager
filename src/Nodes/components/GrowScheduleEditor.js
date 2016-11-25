e8733cf39d0b1ab6fbe58fdad5d68e9d8f1a7356
Kevin Yeh <keyeh@calpoly.edu>
2016-11-24 17:20:14 -0800

    fix grow schedule editor on empty grow schedule

--- src/Nodes/components/GrowScheduleEditor.js
+++ src/Nodes/components/GrowScheduleEditor.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { fetchNodes } from '../actions/nodes_actions';
 import { Link } from 'react-router';
 import Moment from 'react-moment';
+import _ from 'lodash'
 
 class GrowScheduleEditor extends Component {
     
@@ -18,18 +19,22 @@ class GrowScheduleEditor extends Component {
 
 
     renderTimeRanges() {
-        return this.props.grow_schedule.map((timeRange) => {
-            return (
-                    <div>
-                        <strong>
-                            From <Moment unix>{timeRange.from}</Moment> to <Moment unix>{timeRange.to}</Moment> 
-                        </strong>
-                        <ul>
-                            {this.renderEnsures(timeRange.ensure)}
-                        </ul>
-                    </div>
-            );
-        });
+        if (_.isEmpty(this.props.grow_schedule)) {
+            return <strong>No grow schedule loaded.</strong>
+        } else {
+            return this.props.grow_schedule.map((timeRange) => {
+                return (
+                        <div>
+                            <strong>
+                                From <Moment unix>{timeRange.from}</Moment> to <Moment unix>{timeRange.to}</Moment> 
+                            </strong>
+                            <ul>
+                                {this.renderEnsures(timeRange.ensure)}
+                            </ul>
+                        </div>
+                );
+            });
+        }
     }
 
 
@@ -42,7 +47,7 @@ class GrowScheduleEditor extends Component {
             </div>
         );
     } else {
-        return null;
+        return <div>Loading...</div>;
     }
   }
 }
